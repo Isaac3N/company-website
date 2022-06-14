146d585d5a3a92741e29b5873ad45a55d79b8c8a
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-06-14 18:22:51 +0100

    base setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./public/index.html", "./src/input.css", "./public/output.css"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
