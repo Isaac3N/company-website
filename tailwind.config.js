e0b513fec255a66f2d74964a1b2788dbe7d229d2
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-06-16 09:45:59 +0100

    last working commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./public/index.html", "./src/input.css", "./public/output.css"],
+	content: [
+		"./public/index.html",
+		"./src/input.css",
+		"./public/output.css",
+		"./public/our-subsidiaries.html",
+		"./public/thank-you.html",
+	],
 	theme: {
 		conatiner: {
 			center: true,
